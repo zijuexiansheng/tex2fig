0e4f90fed1799a5da818a68708b60e8f52ecd2f9
zijuexiansheng <zijuexiansheng@gmail.com>
2017-07-26 13:38:37 -0400

    use libexec for scripts and tmpl

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@
 
 
 mkdir -p bin/
-mkdir -p script_dir/
+mkdir -p libexec/
 cp src/tex2fig.py bin/tex2fig
-cp src/tex2fig.sh script_dir/tex2fig.sh
-cp src/tex2fig.tmpl script_dir/tex2fig.tmpl
+cp src/tex2fig.sh libexec/tex2fig.sh
+cp src/tex2fig.tmpl libexec/tex2fig.tmpl
 
 if [ "$#" -eq "1" ]; then
     tex2fig_aux=$1
 else
-    tex2fig_aux=$PWD/script_dir
+    tex2fig_aux=$PWD/libexec
 fi
 
 sed -i 's#=>replace-me<=#'"${tex2fig_aux}"'#' bin/tex2fig
